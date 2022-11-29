81b848ef6f11e951f9d647cf2a87a2c5c052582d
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-11-29 20:23:10 -0300

    Create Dto product implementado

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+    @IsString()
+    @ApiProperty({
+        description: 'Nome do Produto',
+        example: 'Pizza de mussarela',
+    })
     name: string;
+
+    @IsString()
+    @ApiProperty({
+        description: 'Descrição do Produto',
+        example: 'Calabresa & mussarela com borda recheada',
+    })
     description: string;
+
+    @IsNumber({
+        maxDecimalPlaces: 2,
+    })
+    @ApiProperty({
+        description: 'Preço do produto',
+        example: '45.00',
+    })
     price: number;
+
+    @IsUrl()
+    @ApiProperty({
+        description: 'Imagem do produto',
+        example: 'http://www.pizza-de-calabresa-&-mussarela',
+    })
     image: string;
 }
