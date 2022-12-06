ad8fedb97875ddf8708e49675beb2d31bd472340
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-12-05 21:32:08 -0300

    " create " finalizado

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { Product } from '../product/entities/product.entity';
 import { handleError } from '../utils/handle-error.util';
 import { CreateOrderDto } from './dto/create-order.dto';
 
@@ -20,8 +21,35 @@ export class OrderService {
                     number: createOrderDto.tableNumber,
                 },
             },
+            products: {
+                connect: createOrderDto.products.map((productId) => ({
+                    id: productId,
+                })),
+            },
         };
-        return this.prisma.order.create({ data }).catch(handleError);
+        return this.prisma.order
+            .create({
+                data,
+                select: {
+                    id: true,
+                    table: {
+                        select: {
+                            number: true,
+                        },
+                    },
+                    user: {
+                        select: {
+                            name: true,
+                        },
+                    },
+                    products: {
+                        select: {
+                            name: true,
+                        },
+                    },
+                },
+            })
+            .catch(handleError);
     }
 
     findAll() {
