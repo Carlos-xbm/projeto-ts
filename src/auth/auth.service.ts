90500ac1c8a27ff11c8617c8e3b48c5d6f406a8d
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-12-07 00:13:49 -0300

    Autenticação e token de login implementado

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
+    constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) {}
+
     async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+        const { nickname, password } = loginDto;
+
+        //Prouca e checa se usuario existe, usando nickname
+        const user = await this.prisma.user.findUnique({ where: { nickname } });
+
+        if (!user) {
+            throw new UnauthorizedException('Usuário não encontrado');
+        }
+
+        //Valida se a senha esta correta
+        const isHashValid = await bcrypt.compare(password, user.password);
+
+        if (!isHashValid) {
+            throw new UnauthorizedException('Usuário e/ou senha invalido');
+        }
+
+        delete user.password;
         return {
-            token: 'teste',
-            user: undefined,
+            token: this.jwtService.sign({ nickname }),
+            user,
         };
     }
 }
--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -4,7 +4,8 @@ import { User } from '../../user/entities/user.entity';
 export class LoginResponseDto {
     @ApiProperty({
         description: 'jwt gerado pelologin',
-        example: 'TOKEN_GERADO_AUTOMATICAMENTE',
+        example:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImNhcmxvcy14Ym0iLCJpYXQiOjE2NzAzODIzMzcsImV4cCI6MTY3MDQ2ODczN30.M56L0hzO4qkcJ2F3wrlM0ncF1XFJclWDpHAQmswja18',
     })
     token: string;
 
