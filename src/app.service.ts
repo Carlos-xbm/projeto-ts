bc89598090a6534bf17e2fbc0a8321a34001434e
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-12-08 20:37:33 -0300

    ' return ' atualizado

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getAppStatus(): string {
-    return 'Hello World!';
-  }
+    getAppStatus(): string {
+        return 'Olá! meu nome é Carlos Eduardo Carvalho \n Projeto de Back-End com TypeScript, modulo 05 Full-Stack. \n https://projeto-ts-production.up.railway.app/api';
+    }
 }
