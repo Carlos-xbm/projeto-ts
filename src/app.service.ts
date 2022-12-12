cbc47aa6504fb85bbd9b5e13567d71df0464b771
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-12-12 18:57:31 -0300

    ' getAppStatus ' atualizado

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    getAppStatus(): string {
-        return 'Olá! meu nome é Carlos Eduardo Carvalho \n Projeto de Back-End com TypeScript, modulo 05 Full-Stack. \n https://projeto-ts-production.up.railway.app/api';
+    getAppStatus(baseUrl: string) {
+        return {
+            status: 'Servidor rodando!',
+            docs: baseUrl + '/api',
+        };
     }
 }
